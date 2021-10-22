e9fe12353bc5fa8374f50f96bdc1306b1a5e1d4f
pedrofrohmut <pedrofrohmut@gmail.com>
2021-10-22 19:42:21 -0300

    xmonad updates

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,49 +5,32 @@ import System.Exit
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Util.SpawnOnce
+import XMonad.Util.Loggers
 
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 
+
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- Bar imports
+-- XMobar imports
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 
--- Main Function --------------------------------------------------------------------------------
+-- Extending
+import XMonad.Actions.CycleRecentWS
+import XMonad.Actions.CycleWS
+
+import XMonad.Layout.NoBorders
+
 main :: IO ()
 main = xmonad 
+     . ewmhFullscreen 
      . ewmh 
-     =<< statusBar "xmobar" def toggleStrutsKey myConfig
-  where
-    toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
-    toggleStrutsKey XConfig{ modMask = m } = (m, xK_b)
-
--- LayoutHook -----------------------------------------------------------------------------------
-myLayouts = tiled ||| Mirror tiled ||| Full
-  where
-    nmaster = 1      -- Default number of windows in the master pane
-    delta   = 3/100  -- Percent of screen to increment by when resizing panes
-    ratio   = 1/2    -- Default proportion of screen occupied by master pane
-    tiled   = Tall nmaster delta ratio
-
--- Manage hook ----------------------------------------------------------------------------------
--- use $ xprop | grep WM_CLASS then click to get the name to type
-myManageHook = composeAll
-  [ className =? "mpv" --> doFloat
-  , isDialog           --> doFloat
-  ]
-
--- Handle event hook ----------------------------------------------------------------------------
-myHandleEvenHook = fullscreenEventHook
-
--- Log hook -------------------------------------------------------------------------------------
-myLogHook = return ()
-
--- Startup hook ---------------------------------------------------------------------------------
-myStartupHook = do
-  spawnOnce "trayer --edge top --align right --height 18 --SetPartialStrut true --width 5 --padding 4 --tint #212121"
+     . withEasySB(statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
 
 -- My Configuration -----------------------------------------------------------------------------
 myConfig = def 
@@ -74,6 +57,11 @@ myConfig = def
       ((mod4Mask, xK_q),   kill) 
     , ((mod4Mask .|. shiftMask, xK_Return), windows W.swapMaster)
 
+    -- Workspaces
+    , ((mod4Mask, xK_Tab), toggleRecentWS)
+    , ((mod4Mask, xK_h),   prevWS)
+    , ((mod4Mask, xK_l),   nextWS)
+
     -- Apps
     , ((mod4Mask, xK_Return), spawn "alacritty")
     , ((mod4Mask, xK_p),      spawn "rofi -modi drun -show drun -theme ~/.config/rofi/themes/my_dracula.rasi")
@@ -87,7 +75,62 @@ myConfig = def
 
     -- Session managment
     , ((mod4Mask .|. shiftMask, xK_z),  spawn "i3lock -i /home/pedro/media/images/wallpaper/lock.png -u")
-    , ((mod4Mask .|. shiftMask, xK_r),  spawn "killall xmobar; killall trayer; xmonad --recompile; xmonad --restart")
+    , ((mod4Mask .|. shiftMask, xK_r),  spawn "killall xmobar; xmonad --recompile; xmonad --restart")
     , ((mod4Mask .|. shiftMask, xK_F3), spawn "systemctl suspend")
     , ((mod4Mask .|. shiftMask, xK_F4), io (exitWith ExitSuccess))
     ]
+
+-- LayoutHook -----------------------------------------------------------------------------------
+myLayouts = smartBorders(tiled ||| Mirror tiled ||| Full)
+  where
+    nmaster = 1      -- Default number of windows in the master pane
+    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+    ratio   = 1/2    -- Default proportion of screen occupied by master pane
+    tiled   = Tall nmaster delta ratio
+
+-- Manage hook ----------------------------------------------------------------------------------
+-- use $ xprop | grep WM_CLASS then click to get the name to type
+myManageHook = composeAll
+  [ className =? "mpv"  --> doFloat
+  , className =? "Gimp" --> doFloat
+  , isDialog            --> doFloat
+  ]
+
+-- Handle event hook ----------------------------------------------------------------------------
+myHandleEvenHook = mempty
+-- myHandleEvenHook = fullscreenEventHook
+
+-- Log hook -------------------------------------------------------------------------------------
+myLogHook = return ()
+
+-- Startup hook ---------------------------------------------------------------------------------
+myStartupHook = return ()
+
+-- XMobar ---------------------------------------------------------------------------------------
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = white . wrap (white " [") (white "]")
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = gray . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- Windows should have *some* title, which should not not exceed a sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+    gray     = xmobarColor "#656565" ""
