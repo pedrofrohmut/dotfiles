f535ccbc9d963fde5214d844d3b72e31d64cbb27
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-08-09 14:42:25 -0300

    Vim alias to work on luaconfig

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,17 @@ if status is-interactive
 end
 
 # --- ALIASES
-alias vim='nvim'
-alias v='nvim'
+alias vim='nvim -u ~/programming/nvimconfig/main.vim'
+#alias v='nvim'
 #alias evim='nvim -u ~/programming/nvimconfig-lua/init.lua'
+
 alias gits='git status'
 alias gitl='git log -n 5'
+
 alias ll='ls -lA'
 alias cll='clear; ls -lA'
 alias tree='tree -C'
+alias jj='jobs'
 
 # --- Starship Prompt
 starship init fish | source
