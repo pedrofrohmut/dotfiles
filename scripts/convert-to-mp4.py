8fe32a38dd6a53a24e310fdaaa99bd6fce093709
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-01-11 11:47:24 -0300

    Ajusted values for convert to mp4 after lots of tests

--- scripts/convert-to-mp4.py
+++ scripts/convert-to-mp4.py
@@ -3,22 +3,31 @@
 import subprocess
 import argparse
 
-# ffmpeg -i musializer.webm -c:v libx264 -c:a aac -b:a 128k -b:v 5 -crf 23 -f mp4 musializer-1.mp4
+# ffmpeg -i <input.webm> -preset fast -c:v libx265 -b:v 1M \
+#        -c:a aac -strict experimental -crf 31 -vf scale=-1:720 -r 30 -f mp4 <output.mp4>
 
 def convert_webm_to_mp4(input_file, output_file):
     command = [
         'ffmpeg',
         '-i', input_file,
-        '-c:v', 'libx264', '-c:a', 'aac', # video and audio codecs
-        '-strict', 'experimental',        # for audio codec
-        '-b:a', '128k',                   # audio bit rate
-        '-b:v', '5',                      # video quality 0 best 10 worst
-        '-crf', '23',                     # set frame rate
-        '-f', 'mp4',                      # set format
+        '-preset', 'fast',         # output file is a little bigger but process is faster
+        '-c:v', 'libx265',         # video codec (MP4 - libx264, HVEC - libx265)
+        '-b:v', '1M',              # video bitrate
+        '-c:a', 'aac',             # audio codec
+        '-b:a', '192k',            # audio bitrate
+        '-strict', 'experimental', # for audio codec
+        '-crf', '31',              # set frame rate (default: 28)
+        '-vf', 'scale=-1:720',     # scale the resolution (-1 for auto width, preserve the ratio)
+        '-r', '30',                # fps
+        '-f', 'mp4',               # set format
         output_file
     ]
 
-    subprocess.run(command)
+    try:
+        print("[Convert To MP4] Start convertion.")
+        subprocess.run(command)
+    except KeyboardInterrupt:
+        print("[Convert To MP4] Convertion interrupt.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert WebM to MP4 using FFmpeg')
