72735a65147ce875baa3eb3ad458e7d517c818f9
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-03-11 09:20:50 -0300

    First configuration of sway with waybar, fcitx, system tray, etc working. Funcionality set, styles will be made on a later commit

--- sway/autostart.sh
+++ sway/autostart.sh
@@ -1,3 +1,44 @@
-firefox &
-thunar &
-# swaybg -m stretch -i $HOME/media/wallpaper/1316292.jpeg &
+################################################################################
+# autostart for Sway ###########################################################
+################################################################################
+
+#! /usr/bin/env bash
+
+# --- ENV ----------------------------------------------------------------------
+
+export SHELL=/usr/bin/zsh
+export EDITOR=/usr/local/bin/nvim
+export GIT_EDITOR=/usr/local/bin/nvim
+export PATH=$PATH:$HOME/.local/bin
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+
+# --- System Tray --------------------------------------------------------------
+
+# PulseAudio
+pasystray &
+
+# Japanese typing
+fcitx5 &
+
+# Network (Manage your network connections)
+nm-applet &
+
+# --- Background Apps ----------------------------------------------------------
+
+# Change color temperature
+gammastep &
+
+# Hide Mouse Cursor when idle for 2 seconds
+unclutter --timeout 2 --ignore-scrolling &
+
+# PolicyKit Authentication Agent (PolicyKit Authentication Agent)
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Emacs &
+emacs --daemon &
+
+# Waybar for sway
+waybat &
